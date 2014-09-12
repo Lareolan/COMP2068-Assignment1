f5ac606ea05fe5fb035eb075a3dac125269afec9
Lareolan <konstantin_koton@yahoo.com>
2014-09-12 16:05:06 -0400

    Added Media Query prototypes to the project

--- public_html/JavaScript/main.js
+++ public_html/JavaScript/main.js
@@ -10,42 +10,50 @@ $(document).ready(function() {
     $("#navigation li").click(function() {
         var $this = $(this);
         var $previous = $this.siblings().filter(".selected");
-        var timeout = 2000;
-        
+        var timeout = 1000;
+
+        if ($this.is(".disabled") || $this.is(".selected")) {
+            return;
+        }
+
+        $this.siblings().addBack().addClass("disabled");
         hidePanel($previous, $panel, timeout);
         setTimeout(function() {
             $this.addClass("selected");
             $previous.removeClass("selected");
             showPannel($this, $panel, timeout);
         }, timeout*1.1);
+
+        setTimeout(function() {
+            $this.siblings().addBack().removeClass("disabled");
+        }, timeout*2.1);
     });
 });
 
 function hidePanel($nav, $panel, timeout) {
     var navTop = $nav.offset().top;
-    var navBottom = $nav.outerHeight() + navTop;
+    var navBottom = $nav.innerHeight() + navTop;
     var navLeft = $nav.offset().left;
     var navWidth = $nav.outerWidth();
-    
+
     var panelTop = $panel.offset().top;
     var panelBottom = $panel.outerHeight() + panelTop;
     var panelLeft = $panel.offset().left;
     var panelWidth = $panel.outerWidth();
-    
+
     var $animatedPanel = $panel.clone().appendTo("body");
     $animatedPanel.css({
         position: "absolute",
         top: panelTop,
         bottom: panelBottom,
         left: panelLeft,
-/*        right: panelRight, */
         width: panelWidth,
         margin: "0",
         "border-radius": "8px"
     });
 
     $panel.hide();
-    
+
     $animatedPanel.animate({
         top: navTop,
         bottom: navBottom,
@@ -66,16 +74,16 @@ function hidePanel($nav, $panel, timeout) {
 
 function showPannel($nav, $panel, timeout) {
     var navTop = $nav.offset().top;
-    var navBottom = $nav.outerHeight() + navTop;
+    var navBottom = $nav.innerHeight() + navTop;
     var navLeft = $nav.offset().left;
     var navWidth = $nav.outerWidth();
-    
+
     $panel.show();
     var panelTop = $panel.offset().top;
     var panelBottom = $panel.outerHeight() + panelTop;
     var panelLeft = $panel.offset().left;
     var panelWidth = $panel.outerWidth();
-    
+
     var $animatedPanel = $panel.clone().appendTo("body");
     $panel.hide();
 
