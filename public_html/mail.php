d0bff288591db45c5225cbd2db0bdcaa9ba95cb1
Lareolan <konstantin_koton@yahoo.com>
2014-09-19 01:21:14 -0400

    Sending a form e-mail from the contact page is now working

---
+++ public_html/mail.php
@@ -0,0 +1,25 @@
+<?php
+
+    $to = "200278361@student.georgianc.on.ca";
+
+    $subject = $_POST["subject"];
+
+    $headers = "From: " . strip_tags($_POST["email"]) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($_POST["email"]) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $message = "<html><body>";
+    $message .= "<table rules=\"all\" style=\"border-color: #666;\" cellpadding=\"10\">";
+    $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST["name"]) . "</td></tr>";
+    $message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST["email"]) . "</td></tr>";
+    $messageBody = htmlentities($_POST["message"]);
+    if (($messageBody) != '') {
+        $message .= "<tr><td><strong>Message Content:</strong> </td><td>" . $messageBody . "</td></tr>";
+    }
+    $message .= "</table>";
+    $message .= "</body></html>";
+
+    mail($to, $subject, $message, $headers);
+    
+?>
